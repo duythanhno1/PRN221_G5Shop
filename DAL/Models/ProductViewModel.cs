5e082384f4518081e077dfc2596e365bf424a0bf
hoangphan636 <57717259+hoangphan636@users.noreply.github.com>
2023-10-23 21:11:49 +0700

    9:11

---
+++ DAL/Models/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+        public List<Product> Products { get; set; }
+
+      
+    }
+}
